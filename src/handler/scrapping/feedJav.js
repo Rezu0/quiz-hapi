7fb564a74872417669b648c289c5e43f5fa7e91e
Rezu0 <yunianrezky031@gmail.com>
2025-07-31 16:23:43 +0700

    feat: refactor javDesuUncensored and javDesuMosaic scraping logic for improved data extraction

--- src/handler/scrapping/feedJav.js
+++ src/handler/scrapping/feedJav.js
@@ -47,16 +47,41 @@ const javDesuUncensored = async (request, h) => {
     const data = await res.text();
     const $ = cheerio.load(data);
     // javdesu uncensored
-    const title = $('.featured-thumb.grid-img.col-md-12').children().map((index, element) => $(element).attr('title')).get();
-    const link = $('.featured-thumb.grid-img.col-md-12').children().map((index, element) => $(element).attr('href')).get();
-    const image = $('.featured-thumb.grid-img.col-md-12 a').children().map((index, element) => $(element).attr('src')).get();
+
+    const gridSelector = '.grid.gap-x-4.gap-y-8.grid-cols-1.sm\\:grid-cols-2.lg\\:grid-cols-3.xl\\:grid-cols-4';
+
+    const scrapingTitle = $(gridSelector).children().map((index, element) => {
+      const title = $(element).find('h3').text().trim();
+
+      return title;
+    }).get();
+
+    const scrapingLink = $(gridSelector).children().map((index, element) => {
+      const link = `https://javdesu.tv${$(element).find('a').attr('href')}`;
+
+      return link;
+    }).get();
+
+    const scrapingImage = $(gridSelector).children().map((i, el) => {
+      const imageSet = $(el).find('img').attr('srcset');
+
+      let image = null;
+      if (imageSet) {
+        // eslint-disable-next-line no-shadow
+        const imageTrim = imageSet.split(',').map((i) => i.trim());
+        image = `https://javdesu.tv${imageTrim.reverse()[0]?.split(' ')[0]}`; // ambil URL-nya aja
+      }
+
+      return image;
+    }).get();
+
     const response = h.response({
       status: 'success',
       message: 'API Feed Javdesu Uncensored',
       data: {
-        _title: title.reverse(),
-        _link: link.reverse(),
-        _image: image.reverse(),
+        _title: scrapingTitle.reverse(),
+        _link: scrapingLink,
+        _image: scrapingImage,
       },
     });
     response.code(200);
@@ -135,16 +160,40 @@ const javDesuMosaic = async (request, h) => {
     const data = await res.text();
     const $ = cheerio.load(data);
     // javdesu-mosaic
-    const title = $('.featured-thumb.grid-img.col-md-12').children().map((index, element) => $(element).attr('title')).get();
-    const link = $('.featured-thumb.grid-img.col-md-12').children().map((index, element) => $(element).attr('href')).get();
-    const image = $('.featured-thumb.grid-img.col-md-12 a').children().map((index, element) => $(element).attr('src')).get();
+
+    const gridSelector = '.grid.gap-x-4.gap-y-8.grid-cols-1.sm\\:grid-cols-2.lg\\:grid-cols-3.xl\\:grid-cols-4';
+
+    const scrapingTitle = $(gridSelector).children().map((index, element) => {
+      const title = $(element).find('h3').text().trim();
+
+      return title;
+    }).get();
+
+    const scrapingLink = $(gridSelector).children().map((index, element) => {
+      const link = `https://javdesu.tv${$(element).find('a').attr('href')}`;
+
+      return link;
+    }).get();
+
+    const scrapingImage = $(gridSelector).children().map((index, element) => {
+      const imageSet = $(element).find('img').attr('srcset');
+
+      let image = null;
+      if (imageSet) {
+        const imageTrim = imageSet.split(',').map((i) => i.trim());
+        image = `https://javdesu.tv${imageTrim.reverse()[0]?.split(' ')[0]}`;
+      }
+
+      return image;
+    }).get();
+
     const response = h.response({
       status: 'success',
       message: 'API Feed Javdesu Mosaic',
       data: {
-        _title: title.reverse(),
-        _link: link.reverse(),
-        _image: image.reverse(),
+        _title: scrapingTitle.reverse(),
+        _link: scrapingLink,
+        _image: scrapingImage,
       },
     });
     response.code(200);
